d508913721b2595ee2bdd5a5fac4612271b874cf
Amit Sinha <amit.a.sinha@ericsson.com>
2021-02-15 06:13:58 +0100

    Initial Commit

---
+++ competition-game-web-services/src/main/java/com/competition/game/rest/webservices/CompetitionGameApplication.java
@@ -0,0 +1,13 @@
+package com.competition.game.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionGameApplication.class, args);
+	}
+
+}
---
+++ competition-game-web-services/src/test/java/com/competition/game/rest/webservices/CompetitionGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.competition.game.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
