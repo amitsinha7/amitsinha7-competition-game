827662fee0407b332c421ca9be817cb64068acc8
Amit Sinha <amit.a.sinha@ericsson.com>
2021-02-23 13:11:45 +0100

    Working except topplayer

--- competition-game-frontend/src/App.js
+++ competition-game-frontend/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import GameApp from "./components/competition-game/GameApp";
-import "./App.css";
-import "./bootstrap.css";
+import React, { Component } from "react"
+import GameApp from "./components/competition-game/GameApp"
+import "./App.css"
+import "./bootstrap.css"
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <GameApp />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <GameApp />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- competition-game-frontend/src/components/competition-game/TopPlayer.jsx
+++ competition-game-frontend/src/components/competition-game/TopPlayer.jsx
@@ -1,59 +1,62 @@
-import React, { Component } from "react";
-import ChallengeAPI, { JWT_TOKEN } from "../../api/ChallengeAPI.js";
+import React, { Component } from "react"
+import ChallengeAPI, { JWT_TOKEN } from "../../api/ChallengeAPI.js"
 
 class TopPlayer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      players: [],
-      message: null,
-      hasLoginFailed: false,
-      showSuccessMessage: false
-    };
-    this.getTopPlayers = this.getTopPlayers.bind(this);
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            players: [],
+            message: null,
+            hasLoginFailed: false,
+            showSuccessMessage: false
+        }
+        this.getTopPlayers = this.getTopPlayers.bind(this)
+    }
 
-  getTopPlayers() {
-    ChallengeAPI.getTopPlayers(sessionStorage.getItem(JWT_TOKEN))
-      .then(response => {
-        this.setState({ players: response.data });
-        this.props.history.push(`/challenge`);
-      })
-      .catch(() => {
-        this.setState({ showSuccessMessage: false });
-        this.setState({ hasLoginFailed: true });
-      });
-  }
+    componentDidMount() {
+        this.getTopPlayers()
+    }
 
-  render() {
-    console.log("render");
-    return (
-      <div>
-        <h1>Top Players</h1>
-        {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
-        <div className="container">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Nick Name</th>
-                <th>SUCCESS SOLUTIONS</th>
-                <th>TASKS</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.players.map(player => (
-                <tr key={player.id}>
-                  <td>{player.nickname}</td>
-                  <td>{}</td>
-                  <td>{player.toString()}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
+    getTopPlayers() {
+        ChallengeAPI.getTopPlayerAPI(sessionStorage.getItem(JWT_TOKEN))
+            .then(response => {
+                this.setState({ players: response.data.topPlayers })
+            })
+            .catch(() => {
+                this.setState({ showSuccessMessage: false })
+                this.setState({ hasLoginFailed: true })
+            })
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <div>
+                <h1>Top Players</h1>
+                {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Nick Name</th>
+                                <th>SUCCESS SOLUTIONS</th>
+                                <th>TASKS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.players.map(player => (
+                                <tr key={player.id}>
+                                    <td>{player.id}</td>
+                                    <td>{player.successfulAttempt}</td>
+                                    <td>{player.solvedTasks}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default TopPlayer;
+export default TopPlayer
